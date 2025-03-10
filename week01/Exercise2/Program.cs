6fb75c6e41b21be9cb4142ab3b4c4e504737e37e
Kopano-Damane-BYUI <kdamane@byupathway.edu>
2025-03-10 02:57:31 +0200

    I have finished the first 3 exercises in week 1, only 2 more remaining

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your GPA? ");
+        String gpa = Console.ReadLine();
+        int gpaNum = int.Parse(gpa);
+        Console.WriteLine();
+
+        String letter;
+        if (gpaNum >= 90)
+        {
+            letter = "A";
+        }
+        else if (gpaNum >= 80)
+        {
+            letter = "B";
+        }
+        else if (gpaNum >= 70)
+        {
+            letter = "C";
+        }
+        else if (gpaNum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+        Console.WriteLine();
+
+        if (gpaNum >= 70)
+        {
+            Console.Write("Your GP is >= 70: Congratulations, You have passed this course.");
+        }
+        else
+        {
+            Console.Write("Your GP is below 70: Sorry, You have not made it through this course. You need to practise more and you will pass next time!!");
+        }
+
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int mNum = random.Next(1, 101);
+
+        Console.Write("What is your guess? ");
+        string num2 = Console.ReadLine();
+        int guess = int.Parse(num2);
+
+        while (guess != mNum)
+        {
+            if (guess < mNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > mNum)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            // Ask for another guess inside the loop
+            Console.Write("What is your guess? ");
+            num2 = Console.ReadLine();
+            guess = int.Parse(num2);  // Update the guess
+        }
+
+        Console.WriteLine("You guessed it!");
+
     }
 }
